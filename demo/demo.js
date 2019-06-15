b9357dd2d5d65945e0f331b5490b2100a71bd761
Adam Cupiał <adam.cupial@dazn.com>
2019-06-15 22:03:19 +0200

    extended demo - styles and custom validator

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,10 @@
 (function () {
   var fv = new window.FormValidator(document.getElementById('form'));
+  fv.addValidator('name', (field) => {
+    if (field.value === 'dsddd') {
+      return null;
+    } else {
+      return 'Error';
+    }
+  });
 }());
